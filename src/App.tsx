8d289909b7bbc9dd8febaad24fdfd84b3cc0a218
Yobi Bina Setiawan <yobi.studio@gmail.com>
2021-07-29 21:36:24 +0700

    add scss module

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "antd/dist/antd.css";
-import "./assets/css/App.css";
+import "./assets/scss/App.scss";
 
 import Login from "./pages/Auth/Login";
 
--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,44 +1,58 @@
 import React from "react";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Layout, Row, Col } from "antd";
+
+const { Footer, Content } = Layout;
+
+import s from "./style.module.scss";
 
 const Page = () => {
+  const rules = {
+    email: "Invalid E-mail",
+    required: "This is required",
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <Form layout="vertical" initialValues={{ remember: true }}>
-        <Form.Item
-          name="email"
-          label="Email"
-          rules={[
-            {
-              type: "email",
-              message: "The input is not valid E-mail!",
-            },
-            {
-              required: true,
-              message: "Please input your email!",
-            },
-          ]}
-        >
-          <Input size="large" placeholder="Email Address" />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password size="large" placeholder="Password" />
-        </Form.Item>
-        <Form.Item>
-          <div>
-            <Button type="link">Forgot your password?</Button>
-            <Button type="primary" htmlType="submit" size="large">
-              Submit
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
-    </div>
+    <Layout>
+      <Content>
+        <Row>
+          <Col span={24}>
+            <div className={s.login_page}>
+              <h1>Login</h1>
+              <Form layout="vertical" initialValues={{ remember: true }}>
+                <Form.Item
+                  name="email"
+                  label="Email"
+                  rules={[
+                    { required: true, message: rules.required },
+                    { type: "email", message: rules.email },
+                  ]}
+                >
+                  <Input size="large" placeholder="Email Address" />
+                </Form.Item>
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[{ required: true, message: rules.required }]}
+                >
+                  <Input.Password size="large" placeholder="Password" />
+                </Form.Item>
+                <Form.Item>
+                  <div className="d-flex justify-content-between">
+                    <Button type="link" style={{ paddingLeft: 0 }}>
+                      Forgot your password?
+                    </Button>
+                    <Button type="primary" htmlType="submit" size="large">
+                      Submit
+                    </Button>
+                  </div>
+                </Form.Item>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Content>
+      <Footer className="text-center">Project Name</Footer>
+    </Layout>
   );
 };
 
